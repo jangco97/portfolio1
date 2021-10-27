cc82e08b68efe9934430446a89495305a396c771
jangco97 <67136116+jangco97@users.noreply.github.com>
2021-10-27 21:39:03 +0900

    JS: Navbar

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world!');
+'use strict';
+
+//navbar를 투명하다가 내려가면 색깔이 생기도록
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  //   console.log(window.scrollY);
+  //   console.log(`navbarHeight: ${navbarHeight}`);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
