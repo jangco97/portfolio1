1a00a95eff1616184f2f2a927fabae2226315690
jangco97 <67136116+jangco97@users.noreply.github.com>
2021-10-28 17:39:19 +0900

    JS: Navbar toggle + Project UI tweak

--- main.js
+++ main.js
@@ -12,8 +12,12 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
-
-//Hand scrolling when tapping on the navbar
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+//Handle scrolling when tapping on the navbar
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', event => {
   const target = event.target;
@@ -22,6 +26,7 @@ navbarMenu.addEventListener('click', event => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoSelector(link);
 });
 const home = document.querySelector('.home__container');
